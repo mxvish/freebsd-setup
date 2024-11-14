a4001ff21eaff47f8149fba9565727eacd5abb02
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-11-14 12:51:03 +0900

    install sway and wayland

--- freebsd_setup.sh
+++ freebsd_setup.sh
@@ -10,12 +10,14 @@ sysrc kld_list+=i915kms
 pkg update
 pkg upgrade -y
 
-pkg install -y dmenu drm-kmod firefox git i3 i3lock i3status neofetch vim xf86-video-intel xfce4-terminal xorg
+pkg install -y dmenu dmenu-wayland drm-kmod firefox git i3 i3lock i3status neofetch sway vim wayland xf86-video-intel xfce4-terminal xorg
 echo 'exec i3' >  /home/mxvish/.xinitrc
 
 echo 'keymap="us.pc-ctrl"
-linux_enable="YES"' >> /etc/rc.conf
+linux_enable="YES"
+seatd_enable="YES"' >> /etc/rc.conf
 #i3-config-wizard #run this in $USER not in su
+service seatd start
 
 curl -s https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf > /etc/i3status.conf
 #add "i3status -c /etc/i3status.conf" in .config/i3/config
