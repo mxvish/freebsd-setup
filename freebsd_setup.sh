1f5d09cba39f27786fdcf49ef6b8507b14098382
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-01-17 17:56:49 +0900

    configure .vimrc

--- freebsd_setup.sh
+++ freebsd_setup.sh
@@ -21,3 +21,5 @@ service seatd start
 
 curl -s https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf > /etc/i3status.conf
 #add "i3status -c /etc/i3status.conf" in .config/i3/config
+
+curl -s https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc > /home/$USER/.vimrc
