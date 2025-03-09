841de0cbc13e80c911a26e33c79b5f9559e3e1d9
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-03-09 19:16:44 +0900

    change repo name

--- freebsd_setup.sh
+++ freebsd_setup.sh
@@ -19,7 +19,7 @@ seatd_enable="YES"' >> /etc/rc.conf
 #i3-config-wizard #run this in $USER not in su
 service seatd start
 
-curl -s https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf > /etc/i3status.conf
+curl -s https://raw.githubusercontent.com/mxvish/i3config/main/i3status.conf > /etc/i3status.conf
 #add "i3status -c /etc/i3status.conf" in .config/i3/config
 
 curl -s https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc > /home/mxvish/.vimrc
