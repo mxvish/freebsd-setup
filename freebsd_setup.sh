32d17e3320199d1e17515a2a8fff6fda01a5ea11
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-04-27 19:43:08 +0900

    add installation info

--- freebsd_setup.sh
+++ freebsd_setup.sh
@@ -1,8 +1,3 @@
-#acpiconf -i 0	#battery capacity
-
-#ls /dev/da* #find usb partition
-#mount -t msdosfs /dev/da0p2 /mnt/usb
-
 mkdir /mnt/usb
 pw groupmod video -m mxvish
 sysrc kld_list+=i915kms
@@ -27,3 +22,72 @@ curl -s https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc > /home/mxvish
 CURRENT_USER="mxvish"
 touch /home/$CURRENT_USER/.hushlogin
 touch /root/.hushlogin
+
+#-disc.iso
+#1. Boot Multi User
+#Install
+#continue with default keymap
+#set hostname
+#distribution select
+#    lib32
+#    ports
+#    src
+#partitioning -> manual
+#    type    freebsd-swap
+#    size    4g
+#    mountpoint swap
+#    label   swap
+#    type        freebsd-ufs
+#    size        (your preference)
+#    mountpoint  /
+#    label   root
+#    #created automatically when you create root partition
+#    #type    efi
+#    #size    512m
+#    #mountpoint /boot/efi
+#    #label   boot
+#network installation -> iwlwifi
+#regdomain/country -> no
+#network configuration -> iwlwifi
+#configure ipv4 for this interface ->y
+#dhcp -> y
+#configure ipv6 for this interface -> n
+#resolve configuration -> ok
+#is cmos clock set to UTC? -> yes
+#select timezone
+#system configuration
+#    check dumpdev
+#sysctl hardening
+#    check nothing
+#firmware installation
+#    check gpu~ and wifi~
+#add user accounts -> yes
+#    invite user into other groups: wheel
+#    leave others empty for default    
+#final configuration -> exit
+#manual configuration -> no    
+#complete -> reboot
+
+#add following code to /boot/grub/grub.cfg
+#menuentry "FreeBSD" {
+#    set root=(hd0,gpt1)
+#    chainloader /efi/freebsd/loader.efi
+#    boot
+#}
+
+#acpiconf -i 0	#battery capacity
+
+#gpart show -l
+
+#mount -t ext2fs /dev/nvd0p9 /mnt/gentoo
+
+#mount bsd ufs partition from linux
+#mount -t ufs -o ufstype=ufs2 /dev/sdb1 /mnt/bsd
+
+#ls /dev/da* #find usb partition
+#mount -t msdosfs /dev/da0p2 /mnt/usb
+
+#mixer vol=10%
+#mixer vol=+5%
+
+#check how mac is configured(e.g. sysctl) and apply it on fbsd?
