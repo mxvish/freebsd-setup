dc8195d82deddda7b93d979d29a6348633a7e63a
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-08-23 13:30:09 +0900

    feat: install scfb driver

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ sysrc kld_list+=i915kms
 pkg update
 pkg upgrade -y
 
-pkg install -y btop cmatrix dmenu dmenu-wayland drm-kmod emacs-nox fastfetch git grim i3 i3lock i3status nasm qemu sway tree vim wayland xf86-video-intel xfce4-terminal xorg
+pkg install -y btop cmatrix dmenu dmenu-wayland drm-kmod emacs-nox fastfetch git grim i3 i3lock i3status nasm qemu sway tree vim wayland xf86-video-intel xf86-video-scfb xfce4-terminal xorg
 #echo 'exec i3' >  /home/$(hostname)/.xinitrc
 
 echo 'boot_mute="YES"' >> /boot/loader.conf
