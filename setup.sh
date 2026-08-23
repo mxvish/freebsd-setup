cd71d40f850a6ed9d5837ec4d30bf6a08b6a0a7a
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2026-08-23 13:49:10 +0900

    feat: install firefox

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ sysrc kld_list+=i915kms
 pkg update
 pkg upgrade -y
 
-pkg install -y btop cmatrix dmenu drm-kmod emacs-nox fastfetch git i3 i3lock i3status nasm qemu tree vim xf86-video-intel xf86-video-scfb xfce4-terminal xorg
+pkg install -y btop cmatrix dmenu drm-kmod emacs-nox fastfetch firefox git i3 i3lock i3status nasm qemu tree vim xf86-video-intel xf86-video-scfb xfce4-terminal xorg
 #echo 'exec i3' >  /home/$(hostname)/.xinitrc
 
 echo 'boot_mute="YES"' >> /boot/loader.conf
